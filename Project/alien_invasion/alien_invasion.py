e3721eb687bd0e0afe96b6b7e4e170fec3255086
SohnYick <1404305564@qq.com>
2023-05-31 22:29:54 +0800

    Feat:Game screen and config module.

---
+++ Project/alien_invasion/alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+from settings import Settings
+def run_game():
+
+    # 初始化
+    pygame.init()
+
+    settings = Settings()
+
+    # 创建屏幕并给定标题
+    screen = pygame.display.set_mode((settings.screen_width,settings.screen_height))
+    pygame.display.set_caption(settings.game_title)
+
+    # 游戏主循环
+    while True:
+        # 监听：退出游戏
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(settings.bg_color)
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
+
---
+++ Project/alien_invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """ 存储游戏中所有的设置信息 """
+    def __init__(self):
+        
+        self.game_title = "外星人入侵"
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
+        

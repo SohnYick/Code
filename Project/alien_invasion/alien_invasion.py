4cdd167144709d4d5ac61737888569fc29be61a6
SohnYick <1404305564@qq.com>
2023-06-09 17:51:38 +0800

    Refactor:Game functions module.

--- Project/alien_invasion/alien_invasion.py
+++ Project/alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 def run_game():
 
     # 初始化
@@ -17,15 +18,8 @@ def run_game():
     
     # 游戏主循环
     while True:
-        # 监听：退出游戏
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(settings.bg_color)
-        # 绘制飞船
-        ship.blitme()
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
-
+        gf.check_events()
+        gf.update_screen(settings,screen,ship)
+        
 run_game()
 

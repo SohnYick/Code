47cb9fb0c0d57b9b173e323b9b1fe83fd9742527
SohnYick <1404305564@qq.com>
2023-08-21 21:46:43 +0800

    新增:第一个CPP程序。

---
+++ CPP/1.Hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+		using namespace std;
+		cout << "Hello World!" << endl;
+		cout << "My name is SohnYick." << endl;
+		cout << "Good bye." << endl;
+
+		return 0;
+}

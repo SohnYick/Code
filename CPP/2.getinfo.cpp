52e5b635cd9d84ddbb87793475d389003db0c4ca
SohnYick <1404305564@qq.com>
2023-08-21 22:27:46 +0800

    新增:使用cin获取信息。

---
+++ CPP/2.getinfo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+		using namespace std;
+
+		int number;
+
+		cout << "Money number:";
+
+		cin >> number;
+
+		cout << "You have " << number << " money.";
+
+		return 0;
+}
